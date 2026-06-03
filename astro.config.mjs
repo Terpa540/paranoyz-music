af3ff0acfb2a2d0fd42494d23e95d30563d99e39
Terpa540 <terpa540@gmail.com>
2026-06-03 11:15:46 +0200

    Add facilities section and SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+
+  site: 'https://paranoyz.com',
+
+  integrations: [
+    sitemap()
+  ],
+
   vite: {
     plugins: [tailwindcss()]
   }
+
 });
